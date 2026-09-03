540604b921084fc552b56ef8d3fe6d929f0fa0a1
FluctMember <fluct_g_developer@cartahd.com>
2026-09-03 07:27:17 +0000

    add SDK Ver 6.44.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/voyagegroup/FluctSDK-iOS-Swift-Package.git",
-            exact: "6.44.1"
+            exact: "6.44.2"
         ),
         .package(
             url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
